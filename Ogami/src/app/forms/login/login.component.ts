c42edcd57ffe7fae7d9a9d2d535c41edd9abc4fa
Khoa Ta <tndkhoaa@gmail.com>
2019-09-09 16:02:02 +0700

    feat:  Login Authentication http://redmine.appscyclone.com/issues/42541

--- Ogami/src/app/forms/login/login.component.ts
+++ Ogami/src/app/forms/login/login.component.ts
@@ -1,5 +1,14 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+export interface LoginResponse {
+  success: boolean;
+  message: string;
+  token;
+  user;
+}
 
 @Component({
   selector: 'app-login',
@@ -8,7 +17,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  message;
+  messageClass;
+  processing: boolean;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
   createForm() {
     this.loginForm = this.fb.group({
@@ -39,6 +56,17 @@ export class LoginComponent implements OnInit {
     return this.loginForm.get('password');
   }
 
+  disableForm() {
+    // tslint:disable-next-line:no-string-literal
+    this.loginForm.controls['username'].disable();
+    this.loginForm.controls.password.disable();
+  }
+
+  enableForm() {
+    // tslint:disable-next-line:no-string-literal
+    this.loginForm.controls['username'].enable();
+    this.loginForm.controls.password.enable();
+  }
   getErrorUsernameMessage() {
     return this.loginForm.get('username').hasError('required')
       ? 'You have enter a value'
@@ -58,7 +86,33 @@ export class LoginComponent implements OnInit {
       ? 'No more than 20 characters'
       : '';
   }
+
+  onLoginSubmit() {
+    this.processing = true;
+    this.disableForm();
+    const user = {
+      username: this.loginForm.get('username').value,
+      password: this.loginForm.get('password').value
+    };
+    this.auth.loginUser(user).subscribe((data: LoginResponse) => {
+      if (!data.success) {
+        this.messageClass = 'alert alert-danger';
+        this.message = data.message;
+        this.processing = false;
+        this.enableForm();
+      } else {
+        this.messageClass = 'alert alert-success';
+        this.message = data.message;
+        this.auth.storageUserData(data.token, data.user);
+        setTimeout(() => {
+          this.router.navigate(['/home']);
+        }, 2000);
+      }
+    });
+  }
+
   ngOnInit() {
     this.createForm();
+    this.processing = false;
   }
 }
--- Ogami/src/app/services/auth.service.ts
+++ Ogami/src/app/services/auth.service.ts
@@ -8,30 +8,37 @@ import {
   distinctUntilChanged,
   switchMap
 } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   domain = 'http://localhost:8080';
+  authToken;
+  user;
+
   constructor(private http: HttpClient) {}
 
   registerUser(user) {
     return this.http
       .post(this.domain + '/authentication/register', user)
-      .map(res => res);
+      .pipe(map(res => res));
   }
 
-  checkUsername(username) {
+  loginUser(user) {
     return this.http
-      .get(this.domain + '/authentication/checkUsername' + username)
-      .map(res => res);
+      .post(this.domain + '/authentication/login', user)
+      .pipe(map(res => res));
   }
 
-  checkEmail(email) {
-    return this.http
-      .get(this.domain + '/authentication/checkEmail' + email)
-      .map(res => res);
+  storageUserData(token, user) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+
+    this.authToken = token;
+    this.user = user;
   }
 }
